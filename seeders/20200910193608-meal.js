57c9a813d0c317d80b884a087e2ee29b4abd75a2
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-09-10 15:23:44 -0500

    seed files created and populated

--- seeders/20200910193608-meal.js
+++ seeders/20200910193608-meal.js
@@ -2,15 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let meals = await queryInterface.bulkInsert("Meals", [
+      { name: "Tacos" },
+      { name: "Cheesy Potato Soup" },
+      { name: "Beef Stroganoff" },
+      { name: "Chili Cheese Dogs" },
+      { name: "Spaghetti" },
+      { name: "Cajun Sausage Alfredo" },
+      { name: "Chicken and Dumplings" },
+      { name: "Chicken and Noodles" },
+      { name: "Enchiladas" },
+      { name: "Fajitas" },
+      { name: "Bratwurst" },
+      { name: "Pancakes" },
+      { name: "Breakfast Burritos" },
+      { name: "Omelettes" },
+      { name: "Eggs Benedict" },
+      { name: "Chicken Pot Pie" },
+      { name: "White Chili" },
+      { name: "Grilled Cheese and Tomato Soup" },
+      { name: "Fried Chicken" },
+      { name: "Pizza" },
+      { name: "French Bread Pizza" },
+      { name: "Chinese Stir Fry" },
+      { name: "Pot Roast" },
+      { name: "Red Beans and Rice" },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20200910193625-ingredients.js
+++ seeders/20200910193625-ingredients.js
@@ -2,15 +2,48 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let ingredients = await queryInterface.bulkInsert("Ingredients", [
+      { ingredient: "tortillas", amount: 1, measurement: "package", mealId: 1 },
+      { ingredient: "ground beef or chicken", amount: 1, measurement: "pound",mealId: 1 },
+      { ingredient: "taco seasoning", mealId: 1 },
+      { ingredient: "shredded cheese", mealId: 1 },
+      { ingredient: "optional: lettuce", mealId: 1 },
+      { ingredient: "optional: tomato", mealId: 1 },
+      { ingredient: "optional: onion", mealId: 1 },
+      { ingredient: "optional: salsa", mealId: 1 },
+      { ingredient: "optional: mild sauce", mealId: 1 },
+      { ingredient: "optional: sour cream", mealId: 1 },
+      { ingredient: "optional: chips", mealId: 1 },
+      { ingredient: "optional: guacamole (mix+avocado)", mealId: 1 },
+      { ingredient: "chicken broth", mealId: 2 },
+      { ingredient: "potatos", mealId: 2 },
+      { ingredient: "onion or minced onion seasoning", mealId: 2 },
+      { ingredient: "salt", mealId: 2 },
+      { ingredient: "pepper", mealId: 2 },
+      { ingredient: "celery", mealId: 2 },
+      { ingredient: "bacon", mealId: 2 },
+      { ingredient: "shredded cheese", mealId: 2 },
+      { ingredient: "sliced american cheese", mealId: 2 },
+      { ingredient: "optional: cheddar bay biscuits", mealId: 2 },
+      { ingredient: "egg noodles", mealId: 3 },
+      { ingredient: "ground beef", mealId: 3 },
+      { ingredient: "sour cream", mealId: 3 },
+      { ingredient: "cream of mushroom soup", mealId: 3 },
+      { ingredient: "garlic powder", mealId: 3 },
+      { ingredient: "optional: french bread", mealId: 3 },
+      { ingredient: "hot dogs", mealId: 4 },
+      { ingredient: "hot dog buns", mealId: 4 },
+      { ingredient: "chili or chili sauce", mealId: 4 },
+      { ingredient: "preferred condiments", mealId: 4 },
+      { ingredient: "optional: shredded cheese", mealId: 4 },
+      { ingredient: "optional: onions", mealId: 4 },
+      { ingredient: "spaghetti noodles", mealId: 5 },
+      { ingredient: "optional: white or red sauce", mealId: 5 },
+      { ingredient: "optional: ground beef or italian sausage", mealId: 4 },
+      { ingredient: "optional: french bread", mealId: 5 },
+      { ingredient: "optional: salad fixings", mealId: 5 },
+
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
